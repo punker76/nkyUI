fc778eb1553f33131afa7b4ca06dfe388084a5da
0xFireball <0xFireball@outlook.com>
2016-10-20 22:12:32 -0700

    add metro themes

---
+++ nkyUI/nkyUI.Demo/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Markup.Xaml;
+using nkyUI.Controls;
+
+namespace nkyUI.Demo
+{
+    public class MainWindow : KYUIWindow
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            App.AttachDevTools(this);
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ nkyUI/nkyUI/Util/GeneralUtil.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia;
+using System;
+
+namespace nkyUI.Util
+{
+    public static class GeneralUtil
+    {
+        public static double DistanceTo(this Point p, Point q)
+        {
+            var a = p.X - q.X;
+            var b = p.Y - q.Y;
+            var distance = Math.Sqrt(a * a + b * b);
+            return distance;
+        }
+    }
+}
